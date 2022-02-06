40cfaf0ae0cef6ba88ce828f06f2f2f0d462b69d
Jordanlouie1 <jordanlouie.1@gmail.com>
2022-02-06 03:32:48 -0500

    Add files via upload

--- rpi.py
+++ rpi.py
@@ -1,4 +1,5 @@
 from bluepy import btle
+from bluepy.btle import Scanner, DefaultDelegate
 import os
 from firebase_admin import credentials, initialize_app, storage
 
@@ -10,18 +11,16 @@ picsTaken = 0
 #p.setDelegate( MyDelegate(params))
 
 
-scanner = Scanner().withDelegate(ScanDelegate())
-
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
         DefaultDelegate.__init__(self)
 
     def handleDiscovery(self, dev, isNewDev, isNewData):
-        if dev.addr == "d9:16:66:ba:68:10":
-            os.system("raspistill -o " + filename + str(picsTaken) + ".jpg")
+        if dev.addr == "df:d4:c8:cc:41:d6":
+            os.system("fswebcam " + fileName + ".jpg")
             bucket = storage.bucket()
-            blob = bucket.blog(fileName + str(picsTaken))
+            blob = bucket.blob(fileName + str(picsTaken))
             blob.upload_from_filename(fileName + str(picsTaken) + ".jpg")
             blob.make_public()
             picsTaken += 1
