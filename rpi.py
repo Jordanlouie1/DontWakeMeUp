ffe7fea7af9060f8ee968181ceae24191a130275
Tejesh Anand <tjanand@usc.edu>
2022-02-06 00:01:26 -0800

    use scanner

--- rpi.py
+++ rpi.py
@@ -5,28 +5,31 @@ from firebase_admin import credentials, initialize_app, storage
 
 fileName = 'latest'
 picsTaken = 0
-class MyDelegate(btle.DefaultDelegate):
-    
-    def __init__(self, params):
-        btle.DefaultDelegate.__init__(self)
-        cred = credentials.Certificate("hacksc2022-4d22b-firebase-adminsdk-pymt0-852e6c81ac.json")
-        initalize_app(cred, {'storageBucket': 'hacksck2022-4d22b.appspot.com'})
-        
-        
-    def handleNotification(self, chandle, data):
-        os.system("raspistill -o " + filename + str(picsTaken) + ".jpg")
-        bucket = storage.bucket()
-        blob = bucket.blog(fileName + str(picsTaken))
-        blob.upload_from_filename(fileName + str(picsTaken) + ".jpg")
-        blob.make_public()
-        picsTaken += 1
-        #process
 
-p = btle.Peripheral("d9:16:66:ba:68:10")
-p.setDelegate( MyDelegate(params))
+#p = btle.Peripheral("d9:16:66:ba:68:10")
+#p.setDelegate( MyDelegate(params))
+
+
+scanner = Scanner().withDelegate(ScanDelegate())
+
+
+class ScanDelegate(DefaultDelegate):
+    def __init__(self):
+        DefaultDelegate.__init__(self)
+
+    def handleDiscovery(self, dev, isNewDev, isNewData):
+        if dev.addr == "d9:16:66:ba:68:10":
+            os.system("raspistill -o " + filename + str(picsTaken) + ".jpg")
+            bucket = storage.bucket()
+            blob = bucket.blog(fileName + str(picsTaken))
+            blob.upload_from_filename(fileName + str(picsTaken) + ".jpg")
+            blob.make_public()
+            picsTaken += 1
+        
+
+scanner = Scanner().withDelegate(ScanDelegate())
+
 
 while True:
-    if p.WaitForNotifications(1.0):
-        continue;
-    print("waiting")
+    devices = scanner.scan(10.0)
 
