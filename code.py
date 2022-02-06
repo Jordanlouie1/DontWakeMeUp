1e12727e0875e756839959588c41472272da875e
gdanehy <danehy@usc.edu>
2022-02-05 22:38:45 -0800

    Update code.py

--- code.py
+++ code.py
@@ -15,28 +15,23 @@ ble = BLERadio()
 uart = UARTService()
 advertisement = ProvideServicesAdvertisement(uart)
 
-ble.start_advertising(advertisement)
-
 print("This is BroadcastNet CLUE sensor:", adafruit_ble_broadcastnet.device_address)
 
 while True:
     measurement = adafruit_ble_broadcastnet.AdafruitSensorMeasurement()
-
-
     clue_data[0].text = "Acceleration: {:.2f} {:.2f} {:.2f}".format(*clue.acceleration)
     clue_data[1].text = "Gyro: {:.2f} {:.2f} {:.2f}".format(*clue.gyro)
     clue_data[2].text = "Temperature: {:.1f}C".format(clue.temperature)
     clue_data[3].text = "Humidity: {:.1f}%".format(clue.humidity)
-    measurement = clue.shake(shake_threshold=15, avg_count=10, total_delay=0.1)
     if clue.shake(shake_threshold=15, avg_count=10, total_delay=0.1):
         clue_data[4].text = "Shake: TRUE"
         clue.start_tone(523)
-        #send signal to raspberry pie
+        # send signal to raspberry pie
+        measurement.acceleration = clue.acceleration
         adafruit_ble_broadcastnet.broadcast(measurement)
         time.sleep(10)
-
     else:
         clue_data[4].text = "Shake: FALSE"
         clue.stop_tone()
-    clue_data.show()
 
+    clue_data.show()
